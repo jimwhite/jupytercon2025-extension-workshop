8b8df52d9acd8cabf9de5e1acd0f6b6ac3b8ddcf
Jim White <jim@ifcx.org>
2025-11-03 11:29:01 -0800

    Add command to palette working

--- src/index.ts
+++ src/index.ts
@@ -2,18 +2,25 @@ import {
   JupyterFrontEnd,
   JupyterFrontEndPlugin
 } from '@jupyterlab/application';
-
+import { ICommandPalette } from'@jupyterlab/apputils';
 import { requestAPI } from './request';
+import { ImageCaptionMainAreaWidget } from './widget';
 
 /**
  * Initialization data for the jupytercon2025-extension-workshop extension.
  */
 const plugin: JupyterFrontEndPlugin<void> = {
   id: 'jupytercon2025-extension-workshop:plugin',
-  description: 'A JupyterLab extension that displays a random image and caption.',
+  description: 'A NEWER JupyterLab extension that displays a random image and caption by Jim!',
   autoStart: true,
-  activate: (app: JupyterFrontEnd) => {
-    console.log('NEW, IMPROVED! JupyterLab extension jupytercon2025-extension-workshop is activated!');
+  requires: [ICommandPalette],  // dependencies of our extension
+  activate: (
+    app: JupyterFrontEnd,
+    // The activation method receives dependencies in the order they are specified in
+    // the "requires" parameter above:
+    palette: ICommandPalette    
+  ) => {
+    console.log('EVEN NEWER JupyterLab extension jupytercon2025-extension-workshop is activated!');
 
     requestAPI<any>('hello')
       .then(data => {
@@ -24,6 +31,21 @@ const plugin: JupyterFrontEndPlugin<void> = {
           `The jupytercon2025_extension_workshop server extension appears to be missing.\n${reason}`
         );
       });
+
+    //Register a new command:
+    const command_id = 'image-caption:open';
+    app.commands.addCommand(command_id, {
+      execute: () => {
+        // When the command is executed, create a new instance of our widget
+        const widget = new ImageCaptionMainAreaWidget();
+
+        // Then add it to the main area:
+        app.shell.add(widget, 'main');
+        return widget;
+      },
+      label: 'View an image & caption by Jim'
+    });
+    palette.addItem({ command: command_id, category: 'Tutorial' });
   }
 };
 
